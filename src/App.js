e2648c5ab56bc54e53b956d033bfa1e02ab0aaaa
l <kolipzpzol123@gmail.com>
2023-03-21 20:43:55 +0300

    Create

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [money, setMoney] = useState(0);
+    const [man, setMan] = useState(0);
+    const [resu, setResu] = useState(0);
+    const [tea, setTea] = useState(0);
+    const person = (e) => {
+        setMan(parseInt(e.target.value));
+    };
+    const schet = (e) => {
+        setMoney(parseInt(e.target.value));
+    };
+    return (
+        <div className="wrap">
+            <input onChange={person} type="number" />
+            <input onChange={schet} type="number" />
+            <div className="extra">
+            <p className="qwe">Чаевые</p>
+            <select className="Tea">
+                <option value="60%" >60</option>
+                <option value="40%" >40</option>
+                <option value="20%">20</option>
+            </select>
+            </div>
+           
+            <button
+                className="count"
+                onClick={() => {
+                    let check = money / man;
+                    // let Aet = money * (.value / 100)
+                    setResu(Math.round(check));
+                    setTea(Math.round(Aet))
+                }}
+            >
+                Посчитать
+            </button>
+            <p className="Res">Денег c каждого-{resu}</p>
+            <p className="Tea"> Чаевые-{tea}</p>
+        </div>
+    );
 }
 
 export default App;
